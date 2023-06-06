9bf30ba700a058b2c96cb2df5dca1cbc6cb68f09
Kushpreet <viriak@myumanitoba.ca>
2023-06-05 23:09:21 -0500

    Add basic about info, add skill section

--- app.js
+++ app.js
@@ -3,6 +3,29 @@ const navBtnsContainer = document.querySelector(".nav-btns");
 const allNavBtns = document.querySelectorAll(".nav-btn");
 const mainBody = document.querySelector(".main");
 
+// To avoid constantly updating the HTML, just save static info here.
+let UserData = {
+  "about-role": "Junior Software Developer",
+  "about-description": `As a recent computer science graduate with a deep understanding of SOLID principles and strong technical skills in
+  multiple programming languages, I am passionate about programming and problem-solving. My experience collaborating to
+  develop web applications using VueJS, Dot Net frameworks, and Cloud-based AWS services has equipped me with the ability to
+  create efficient, scalable solutions that meet real-world business needs. I am particularly interested in the intricacies
+  of low-level programming and enjoy uncovering the inner workings of languages and libraries. Very eager to explore new
+  opportunities that allow me to continue pursuing my passions.`,
+};
+function populateUserData() {
+  console.log("called");
+  for (const id in UserData) {
+    if (Object.hasOwnProperty.call(UserData, id)) {
+      const data = UserData[id];
+      const element = document.getElementById(id);
+      if (element) element.textContent = data;
+    }
+  }
+}
+window.onload = populateUserData();
+
+// add listeners for transition between sections
 function sectionTransition() {
   const clearActiveBtns = function () {
     let currActiveBtns = document.querySelectorAll(".active-btn");
@@ -28,5 +51,4 @@ function sectionTransition() {
     });
   });
 }
-
 sectionTransition();
