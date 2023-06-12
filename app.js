dcfd2039e92e9a5e461a5a01bd103d864e78d94c
Kushpreet <viriak@myumanitoba.ca>
2023-06-11 23:01:50 -0500

    Improve Nav bar, animate skills section

--- app.js
+++ app.js
@@ -3,18 +3,27 @@ const navBtnsContainer = document.querySelector(".nav-btns");
 const allNavBtns = document.querySelectorAll(".nav-btn");
 const mainBody = document.querySelector(".main");
 const pdfContainer = document.getElementById("pdfContainer");
+const progressBars = document.querySelectorAll(".progress-bar");
 
 pdfjsLib.GlobalWorkerOptions.workerSrc = "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.7.107/pdf.worker.js";
 
 // To avoid constantly updating the HTML, just save static info here.
 let UserData = {
-  "about-role": "Junior Software Developer",
-  "about-description": `As a recent computer science graduate with a deep understanding of SOLID principles and strong technical skills in
+  about_role: "Junior Software Developer",
+  about_description: `As a recent computer science graduate with a deep understanding of SOLID principles and strong technical skills in
   multiple programming languages, I am passionate about programming and problem-solving. My experience collaborating to
   develop web applications using VueJS, Dot Net frameworks, and Cloud-based AWS services has equipped me with the ability to
   create efficient, scalable solutions that meet real-world business needs. I am particularly interested in the intricacies
   of low-level programming and enjoy uncovering the inner workings of languages and libraries. Very eager to explore new
   opportunities that allow me to continue pursuing my passions.`,
+  skills: {
+    HTML_CSS: 70,
+    Javascript: 60,
+    Python: 55,
+    Java: 85,
+    "C#": 80,
+    "C++": 95,
+  },
 };
 function populateUserData() {
   for (const id in UserData) {
@@ -24,6 +33,9 @@ function populateUserData() {
       if (element) element.textContent = data;
     }
   }
+  document.querySelectorAll(".progress-percentage").forEach((ele) => {
+    ele.textContent = UserData.skills[ele.dataset.id] + "%";
+  });
 }
 window.onload = populateUserData();
 
@@ -43,7 +55,7 @@ function sectionTransition() {
   };
 
   allNavBtns.forEach((btn) => {
-    btn.addEventListener("click", () => {
+    btn.addEventListener("click", (e) => {
       clearActiveBtns();
       btn.classList.add("active-btn");
 
@@ -54,6 +66,7 @@ function sectionTransition() {
 
       console.log(document.getElementById("pdfContainer"));
       setup_section(target);
+      e.preventDefault();
     });
   });
 }
@@ -61,15 +74,12 @@ sectionTransition();
 
 function destroy_section(section) {
   if (section.classList.contains("section2") && section.classList.contains("resume")) {
-    console.log("destroying...");
     const canvas = pdfContainer.querySelector("canvas");
     if (canvas) canvas.remove();
   }
 }
 
 function setup_section(section) {
-  console.log("setting up...");
-
   if (section.classList.contains("section2") && section.classList.contains("resume")) {
     pdfjsLib.getDocument("docs/resume.pdf").promise.then((pdf) => {
       pdf.getPage(1).then((page) => {
@@ -88,3 +98,34 @@ function setup_section(section) {
     });
   }
 }
+
+function setupScrollSkillAnimations() {
+  const animateSkills = function () {
+    progressBars.forEach((bar) => {
+      const animation = bar.animate(
+        [
+          {
+            width: "0%",
+          },
+          {
+            width: UserData.skills[bar.dataset.id] + "%",
+          },
+        ],
+        1500
+      );
+      animation.finished.then((e) => {
+        bar.style.width = UserData.skills[bar.dataset.id] + "%";
+      });
+    });
+  };
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) animateSkills();
+    });
+  });
+
+  observer.observe(document.getElementById("skill-progress-box"));
+}
+
+setupScrollSkillAnimations();
